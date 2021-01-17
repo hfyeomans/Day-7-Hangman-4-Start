cf2d803ad8a4d751a949931c82323385eccef955
hfyeomans <hank.yeomans@me.com>
2021-01-17 02:39:12 +0000

    fixed if staetment to be outside 4 loop

--- main.py
+++ main.py
@@ -88,11 +88,11 @@ while not end_of_game:
     #TODO-2: - If guess is not a letter in the chosen_word,
     #Then reduce 'lives' by 1. 
     #If lives goes down to 0 then the game should stop and it should print "You lose."
-    if letter != guess:
+    if guess not in chosen_word:
         lives -= 1
-        if lives == 0:
-            end_of_game = True
-            print("You lose!")
+    if lives == 0:
+        end_of_game = True
+        print("You lose!")
 
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
